778f2a0ec6f778244d7a112ee22b78e4788a66c9
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-06-04 18:59:03 +0600

    Corrected the component AddTaskForm

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 
-interface AddTaskFormProps {
+interface Props {
     addTask: () => void;
-    setCurrentTask: React.Dispatch<React.SetStateAction<string>>;
+    setCurrentTask: (value: string) => void
     currentTask: string;
 }
 
-const AddTaskForm: React.FC<AddTaskFormProps> = ({addTask, setCurrentTask, currentTask}) => {
-    const clickingSend = (e: React.FormEvent<HTMLFormElement>) => {
+const AddTaskForm: React.FC<Props> = ({addTask, setCurrentTask, currentTask}) => {
+    const
+        clickSend = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         addTask();
         setCurrentTask('');
     };
 
     return (
-        <form className="taskForm" onSubmit={clickingSend}>
+        <form className="taskForm" onSubmit={clickSend}>
             <input
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCurrentTask(e.target.value)}
+                className="input"
                 type="text"
                 placeholder="Добавить новую задачу"
                 value={currentTask}
                 required
             />
-            <button type="submit">
+            <button type="submit" className="btn">
                 Добавить
             </button>
         </form>
